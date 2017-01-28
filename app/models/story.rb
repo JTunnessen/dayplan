902606da6b4cfd8dd617e88d7bda3202d2d0b650
@JimT2 <jtunnessen@gmail.com>
2017-01-27 23:03:57 -0500

    Added devise

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,9 @@
 class Story < ApplicationRecord
+
+    validates_presence_and_uniqueness_of :slug
+    validates_presence_of :description
+	validates_presence_of :reporter
+	validates_presence_of :deadline
+	validates_presence_of :date
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  
+  validates_uniqueness_of :email, :within => 7..255
+
+end
